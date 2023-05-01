0e79d7ddf11f82b6b544df0a29a1be84cd1eb540
Marie <marie.pannier@smail.th-koeln.de>
2023-05-01 21:18:11 +0200

    24h update

--- controllers/Values.js
+++ controllers/Values.js
@@ -1,30 +1,24 @@
 const { application } = require("express");
 const Values = require("../models/Values");
 
-//only the last 24hours!
-
-const today = newDate ()
-const twentyFourHoursAgo = newDate(today.getTime() -24 * 60 * 60 * 1000);
-
-const Values = await Values.find({
-  timestamp: {$gte : twentyFourHoursAgo},
-})
-
 exports.saveValues = async (req, res) => {
   console.log(req.body)
-    try {
-        const ValuesInstance = new Values(req.body)
-        const saveResult = await ValuesInstance.save()
-        res.status(201).json(saveResult)
-    } catch(error) {
-        res.status(500).json(error)
-    }
+  try {
+    const valuesInstance = new Values(req.body)
+    const saveResult = await valuesInstance.save()
+    res.status(201).json(saveResult)
+  } catch(error) {
+    res.status(500).json(error)
+  }
 }
+
 exports.getValues = async (req, res) => {
-    try {
-      const allValues = await Values.find();
-      res.status(201).json(allValues);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  };
+  try {
+    const today = new Date();
+    const twentyFourHoursAgo = new Date(today.getTime() - 24 * 60 * 60 * 1000);
+    const allValues = await Values.find({ timestamp: { $gte: twentyFourHoursAgo } });
+    res.status(201).json(allValues);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
