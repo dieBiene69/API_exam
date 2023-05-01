a28b233d7b073f0432574420d9dc4d7f6b911061
Marie <marie.pannier@smail.th-koeln.de>
2023-05-01 22:46:29 +0200

    update

--- controllers/Values.js
+++ controllers/Values.js
@@ -1,30 +1,31 @@
 const { application } = require("express");
 const Values = require("../models/Values");
 
-
 exports.saveValues = async (req, res) => {
-  console.log(req.body)
-    try {
-        const ValuesInstance = new Values(req.body)
-        const saveResult = await ValuesInstance.save()
-        res.status(201).json(saveResult);
-    } catch(error) {
-        res.status(500).json(error);
-    }
+  console.log(req.body);
+  try {
+    const ValuesInstance = new Values(req.body);
+    const saveResult = await ValuesInstance.save();
+    res.status(201).json(saveResult);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 exports.getValues = async (req, res) => {
-    try {
-  //last 24h
-      const today = new Date();
-      const twentyFourHoursAgo = new Date(today.getTime() - 24 * 60 * 60 * 1000);
-      const values24h = await Values.find({
-        date: { $gte: twentyFourHoursAgo },
-});
-
-      const Values = await Values.find();
+  try {
+    //last 24h
+    const today = new Date();
+    const twentyFourHoursAgo = new Date(today.getTime() - 24 * 60 * 60 * 1000);
+    const values24h = await Values.find({
+      date: { $gte: twentyFourHoursAgo },
+    });
+    if (values24h) {
       res.status(201).json(values24h);
-    } catch (error) {
-      res.status(500).json(error);
+    } else {
+      res.status(404).json("fehler");
     }
-  };
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
